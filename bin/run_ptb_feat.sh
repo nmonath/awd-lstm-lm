e797043c905c5958b69496103c4f45e0cee1e961
Nicholas Monath <nmonath@cs.umass.edu>
2020-03-13 23:18:33 -0400

    feat model

---
+++ bin/run_ptb_feat.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -exu
+
+python main.py \
+--batch_size 20 \
+--data data/penn \
+--dropouti 0.4 \
+--dropouth 0.25 \
+--seed 141 \
+--epoch 500 \
+--save PTB.pt \
+--cuda \
+--num_features 100 \
+--feature_dim 16
