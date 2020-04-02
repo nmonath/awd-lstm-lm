04512d39c443032f1b7d7a20e38dca5640515371
Nicholas Monath <nmonath@cs.umass.edu>
2020-04-02 13:21:36 -0400

    fix

--- model.py
+++ model.py
@@ -119,7 +119,7 @@ class RNNModel(nn.Module):
     def compute_z(self):
         # Z = F.relu(torch.matmul(self.word_emb, torch.transpose(self.feature_emb, 1, 0)) - self.feature_relu_bias)
         # Z = F.relu(F.softmax(torch.matmul(self.word_emb, torch.transpose(self.feature_emb, 1, 0)), dim=1) - self.feature_relu_bias)
-        Z = row_wise_sparsemax.appy(torch.matmul(self.word_emb, torch.transpose(self.feature_emb, 1, 0)))
+        Z = row_wise_sparsemax.apply(torch.matmul(self.word_emb, torch.transpose(self.feature_emb, 1, 0)))
         return Z
 
     def feature_model_sparsity_loss(self, lambda1, lambda2, avg1, avg2):
